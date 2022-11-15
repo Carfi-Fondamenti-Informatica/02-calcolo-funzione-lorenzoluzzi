142dc0a5cf7f84b49d778a3dcfa563ca9cca109d
lorenzoluzzi <117952492+lorenzoluzzi@users.noreply.github.com>
2022-11-15 16:41:05 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int a,b,x,y;
+    cout<<"inserire i valori di a , b, x , y"<<endl;
+    cin>>a>>b>>x>>y;
+    if (x<0 && y>0) {
+        cout<<"z = "<<(a*x)-(b*y);
+    }
+else if (x>=0 && y<=0){
+        cout<<"z = "<<(a*(x*x))-(b*y);
 }
+else cout<<"z= "<<(a*x)+(y*y*b);
+return 0;
+}
+
